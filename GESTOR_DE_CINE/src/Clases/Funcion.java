e226cecd6fccd9b78e1b5fa03113cda3f05e3893
Santiago-Stellatelli <sstellatelli@gmail.com>
2025-10-12 12:35:03 -0300

    Agrego atributos a las clases

--- GESTOR_DE_CINE/src/Clases/Funcion.java
+++ GESTOR_DE_CINE/src/Clases/Funcion.java
@@ -1,7 +1,28 @@
 package Clases;
 
+import java.util.Date;
+
 public class Funcion {
+    private Sala sala;
     private Pelicula pelicula;
+    private Date fechaSalida;
+
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
 
     public Pelicula getPelicula() {
         return pelicula;
--- GESTOR_DE_CINE/src/Clases/Pelicula.java
+++ GESTOR_DE_CINE/src/Clases/Pelicula.java
@@ -2,4 +2,12 @@ package Clases;
 
 public class Pelicula {
     private String nombrePelicula;
+
+    public String getNombrePelicula() {
+        return nombrePelicula;
+    }
+
+    public void setNombrePelicula(String nombrePelicula) {
+        this.nombrePelicula = nombrePelicula;
+    }
 }
